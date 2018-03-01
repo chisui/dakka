5b376f0577bc47bfe9057f90824d949956c92c1b
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-03-01 11:12:33 +0000

    ref passing

--- src/Dakka/Actor.hs
+++ src/Dakka/Actor.hs
@@ -21,17 +21,19 @@
 #-}
 module Dakka.Actor where
 
-import Dakka.Constraints
-
 import "base" Data.Kind ( Constraint )
-import "base" Type.Reflection ( Typeable )
+import "base" Data.Typeable ( Typeable, cast )
 import "base" Data.Proxy ( Proxy(..) )
+import "base" Data.Function ( on )
 
 import "transformers" Control.Monad.Trans.State.Lazy ( StateT, execStateT )
 import "transformers" Control.Monad.Trans.Writer.Lazy ( Writer, runWriter )
+import "base" Control.Monad.IO.Class ( MonadIO( liftIO ) )
 
 import "mtl" Control.Monad.State.Class ( MonadState, modify )
 import "mtl" Control.Monad.Writer.Class ( MonadWriter( tell ) )
+
+import Dakka.Constraints
 
 
 -- | A path of an Actor inside the Actor system.
@@ -49,7 +51,10 @@ newtype Path a = Path Word
 --     * create new actors.
 -- 
 class (Actor a, MonadState a m) => ActorContext (a :: *) (m :: * -> *) | m -> a where
-    {-# MINIMAL create, (send | (!)) #-}
+    {-# MINIMAL self, create, (send | (!)) #-}
+
+    -- | reference to the currently running 'Actor'
+    self :: m (Path a)
 
     -- | Creates a new `Actor` of type 'b' with provided start state
     create :: (Actor b, b :∈ Creates a) => b -> m (Path b)
@@ -79,7 +84,7 @@ deriving instance Show Envelope
 
 
 -- | Encapsulates the intent to create another actor.
-data CreationIntent= forall a. Actor a => CreationIntent a
+data CreationIntent = forall a. Actor a => CreationIntent a
 deriving instance Show CreationIntent
 
 -- | An ActorContext that simply collects all state transitions, sent messages and creation intents.
@@ -88,6 +93,8 @@ newtype MockActorContext a v = MockActorContext
   deriving (Functor, Applicative, Monad, MonadState a, MonadWriter [Either Envelope CreationIntent])
 
 instance Actor a => ActorContext a (MockActorContext a) where
+
+    self = return $ Path 0
 
     create a = do
         tell [Right $ CreationIntent a]
@@ -103,13 +110,23 @@ execMock (MockActorContext ctx) = runWriter . execStateT ctx
 --  Actor  --
 -- ------- --
 
-type Behavior a = forall m. ActorContext a m => Message a -> m ()
+-- | A Behavior of an 'Actor' defines how an Actor reacts to a message given a specific state.
+-- A Behavior may be executed in any 'ActorContext' that has all of the actors 'Capabillities'.
+type Behavior a = forall m. (ActorContext a m, m `ImplementsAll` Capabillities a) => Message a -> m ()
+
+-- | To be able to route values through an actor system these values have provide certain features.
 type RichData a = (Show a, Eq a, Typeable a)
+
 class (RichData a, RichData (Message a), Actor `ImplementedByAll` Creates a) => Actor (a :: *) where
+    -- | 
     type Creates a :: [*]
     type Creates a = '[]
   
     type Message a :: *
+
+    type Capabillities a :: [(* -> *) -> Constraint]
+    type Capabillities a = '[]
+
     behavior :: Behavior a
 
 behaviorOf :: Proxy a -> Behavior a 
@@ -120,19 +137,52 @@ behaviorOf = const behavior
 --   Test   --
 -- -------- --
 
+-- | Utillity function for equality of Typeables.
+-- FIXME move to differenct module.
+(=~=) :: (Typeable a, Typeable b, Eq a) => a -> b -> Bool
+a =~= b = Just a == cast b
+
+-- | Actor with all bells and whistles.
 newtype TestActor = TestActor
     { i :: Int
     } deriving (Show, Eq, Typeable)
 instance Actor TestActor where
   type Message TestActor = String
   type Creates TestActor = '[OtherActor]
+  type Capabillities TestActor = '[MonadIO]
   behavior m = do
-      modify (TestActor . succ . i) 
+      modify (TestActor . succ . i)
+      liftIO $ putStrLn m
       p <- create OtherActor
       p ! Msg m
 
+-- | Actor with custom message type.
+-- This one also communicates with another actor and expects a response.
 newtype Msg = Msg String deriving (Show, Eq, Typeable)
+instance Response Msg where
+  toResponse = Msg
 data OtherActor = OtherActor deriving (Show, Eq, Typeable)
 instance Actor OtherActor where
   type Message OtherActor = Msg
-  behavior m = undefined
+  type Creates OtherActor = '[WithRef]
+  behavior m = do
+      p <- create WithRef
+      a <- self
+      p ! RefMsg a
+
+-- | Actor that handles references to other Actors
+class Response a where
+  toResponse :: String -> a
+data RefMsg = forall a. (Actor a, Response (Message a)) => RefMsg
+    { ref :: Path a }
+deriving instance Show RefMsg
+deriving instance Typeable RefMsg
+instance Eq RefMsg where
+    (RefMsg a) == (RefMsg b) = a =~= b
+
+data WithRef = WithRef deriving (Show, Eq, Typeable)
+instance Actor WithRef where
+    type Message WithRef = RefMsg
+    behavior (RefMsg a) = do
+        a ! toResponse "hello"
+
