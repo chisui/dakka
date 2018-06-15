8dd39ddd22fd8ed74aa5f2dfcb321570f862cd3c
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-06-15 18:18:37 +0200

    thesis initial

---
+++ thesis/default.nix
@@ -0,0 +1,38 @@
+with import <nixpkgs> {};
+
+let
+  eisvogel = pkgs.fetchFromGitHub {
+    owner  = "Wandmalfarbe";
+    repo   = "pandoc-latex-template";
+    rev    = "40b00f300237780f2f5486ca88a18d987ad6bcd2";
+    sha256 = "0g8ljzz4qacvjp38c3d5hraxgm93bxxsb8qkjj61b18ia4jnysxp";
+  };
+  
+  pandoc-include-code = pkgs.haskell.lib.dontCheck pkgs.haskellPackages.pandoc-include-code;
+in
+
+stdenv.mkDerivation {
+  name = "dakka-thesis";
+
+  src = ./..;
+
+  buildInputs = [
+    pandoc
+    pandoc-include-code
+    texlive.combined.scheme-full
+  ];
+  buildPhase = ''
+    pandoc thesis/main.md \
+      --filter pandoc-include-code \
+      --from markdown \
+      --template ${eisvogel}/eisvogel.tex \
+      --listings \
+      --toc \
+      -o result.pdf
+  '';
+  installPhase = ''
+    mkdir $out
+    mv result.pdf $out
+  '';
+}
+
---
+++ thesis/main.md
@@ -0,0 +1,27 @@
+---
+title: "Dependently typed Actor framework in Haskell" 
+author:
+- Philipp Dargel
+date: 2018-??-??
+tags:
+- Bachelor thesis
+- Haskell
+- Actor
+- Dependent types
+# eisvogel attributes
+titlepage: yes
+toc-own-page: yes
+...
+
+# Introduction
+
+The goal of this thesis is to create an Actor framework, simlar to akka for Haskell that leverages 
+
+```haskell
+main :: IO ()
+main = putStrLn "some Haskell Code"
+```
+
+```{include=src/Dakka/Actor.hs}
+```
+
