a62e37515e73bf2121f0c05315cc2ca221d39c8b
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-06-24 19:49:33 +0200

    bump compiler version

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 let
   inherit (nixpkgs) pkgs;
-  drv = pkgs.haskell.packages.ghc842.callPackage (import ./dakka.nix) {};
+  drv = pkgs.haskell.packages.ghc843.callPackage (import ./dakka.nix) {};
 in
   if pkgs.lib.inNixShell then drv.env else drv
 
