f0b8b066255330b7232b092efe4964f5839fb9bc
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-08-21 23:22:16 +0200

    added constraints lib

--- dakka.nix
+++ dakka.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, bytestring, mtl, stdenv, transformers, containers, contravariant, distributed-process, distributed-static, network-transport-tcp, should-not-typecheck, tasty, tasty-quickcheck, tasty-hunit, dependent-map, dependent-sum }:
+{ mkDerivation, base, bytestring, mtl, stdenv, transformers, containers, contravariant, distributed-process, distributed-static, network-transport-tcp, should-not-typecheck, tasty, tasty-quickcheck, tasty-hunit, dependent-map, dependent-sum, constraints }:
   
 mkDerivation {
   pname = "dakka";
   version = "0.0.1";
   src = ./.;
-  libraryHaskellDepends = [ base bytestring transformers mtl contravariant containers distributed-process distributed-static network-transport-tcp should-not-typecheck tasty tasty-quickcheck tasty-hunit dependent-map dependent-sum ];
+  libraryHaskellDepends = [ base bytestring transformers mtl contravariant containers distributed-process distributed-static network-transport-tcp should-not-typecheck tasty tasty-quickcheck tasty-hunit dependent-map dependent-sum constraints ];
   description = "dakka";                                                                                                                   
   license = stdenv.lib.licenses.mit;                                                                                                       
 }
