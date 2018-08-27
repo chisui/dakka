c89e7eaa8da753ed78e6f1d1486bc246e6dd2a56
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-08-27 15:34:57 +0200

    missing file

---
+++ nixpkgs.pinned.nix
@@ -0,0 +1,4 @@
+import (builtins.fetchTarball {
+  url    = "https://github.com/NixOS/nixpkgs/archive/5e3c1c2e327c1109f2bb9d615a0d0358fbb296f6.tar.gz";
+  sha256 = "0ggvf3vgcfjh3mz65y70jdhyrcg1gpa1bfh0plvadqa0lm7yw6w7";
+}) {}
