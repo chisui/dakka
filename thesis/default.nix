2f70e1c89076c5b81dabc08aa55c35ff7089f2b4
Philipp Dargel <chisui@informatik.uni-bremen.de>
2018-09-09 14:35:10 +0200

    formating appendix

--- thesis/default.nix
+++ thesis/default.nix
@@ -16,9 +16,9 @@ mkPandoc.mkPandoc {
   bibliography = ./bibliography.bib;
   filters      = [ pandoc-citeproc ];
   toc          = true;
-  template = mkPandoc.templates.eisvogel;
-  csl      = mkPandoc.csls.journal-of-computer-information-systems;
-  listings = true;
+  template     = mkPandoc.templates.eisvogel;
+  csl          = mkPandoc.csls.journal-of-computer-information-systems;
+  listings     = true;
   top-level-division = "section";
   number-sections = true;
   variables = {
@@ -40,11 +40,11 @@ mkPandoc.mkPandoc {
       {\bf Commit:} & \verb|${commitHash}| \\
       {\bf sha256:} & \verb|${sha256}| \\
     \end{tabular}
-    \end{footnotesize}\\
+    \end{footnotesize}
 
-    To browse the repository at this commit visit\\
+    To browse the repository at this commit visit
 
-    \url{https://github.com/chisui/dakka/tree/${commitHash}}.\\
+    \url{https://github.com/chisui/dakka/tree/${commitHash}}.
 
     To verify the hash run:
 
